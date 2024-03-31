ebc736e7508f9fe28c8f686937a11b38bbcebc2d
Khushi-51 <khushiloginto@gmail.com>
2024-03-31 21:48:38 +0530

    intial-commit

---
+++ starpattern.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		int gap=n-i;
+		for(int j=1;j<=gap;j++)
+			count<<" ";
+		}
+		for(int k=1;k<=i;k++){
+			cout<<="*"
+		}
+		 for(int t=1;t<=i;t++){
+                        cout<<="*"
+                }
+		count<<end1;
+	}
+	return 0;
+
+}
